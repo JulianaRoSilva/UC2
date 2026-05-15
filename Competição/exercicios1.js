343c5e8bec33f33ff6047cf36a027f7e8e415bef
Juliana Silva <rsilvajuju@gmail.com>
2026-05-15 17:54:24 -0300

    Add files via upload

--- Competição/exercicios1.js
+++ Competição/exercicios1.js
@@ -3,8 +3,38 @@ const ask = require('readline-sync');
 let produtos = [];
 
 function cadastrarProduto(nome, preco, precoFab, qtdEstoque){
-    produtos.push([produtos.length + 1, preco, precoFab, qtdEstoque]);
+    produtos.push([produtos.length + 1, nome, preco, precoFab, qtdEstoque]);
 }
+
+function buscarProdutoPorId(id){
+    for(let i = 0; i < produtos.length; i++){
+
+        if (id === produtos[i][0]){
+            return `
+            id: ${produtos[i][0]}
+            Produto: ${produtos[i][1]}
+            Preco: ${produtos[i][2]}
+            Preco Fabricacao: ${produtos[i][3]}
+            Quantidade em estoque: ${produtos[i][4]}
+            `
+        }
+    } return 'Produto não encontrado!'
+}
+
+function buscarProdutoPorNome(nome){
+    let findProd = [];
+
+    for(let i = 0; i < produtos.length; i++){
+        if (nome === produtos[i][1]){
+        findProd.push(produtos[i])
+        }
+    } 
+    return `Produtos encontrados com o nome ${nome}: ${JSON.stringify(findProd)}`
+}
+
+
+
+
 
 let nome = ask.question('nome: ')
 let preco = ask.question('preco: ')
@@ -22,4 +52,6 @@ let qtdEstoque1 = ask.question('qtd estoque: ')
 
 cadastrarProduto(nome1, preco1, precogab1, qtdEstoque1);
 
-console.log(produtos);
+let buscaNome = ask.question('Buscar produto por nome: ')
+
+console.log(buscarProdutoPorNome(buscaNome));
